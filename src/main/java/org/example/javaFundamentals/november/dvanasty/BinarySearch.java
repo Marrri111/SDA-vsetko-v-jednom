8f74723229354461b76be3730ab1acd89e50df47
sklen <sklencarovamaria@gmail.com>
2023-12-03 16:53:27 +0100

    BinarySearch a Recursion

--- src/main/java/org/example/javaFundamentals/november/dvanasty/BinarySearch.java
+++ src/main/java/org/example/javaFundamentals/november/dvanasty/BinarySearch.java
@@ -1,21 +1,35 @@
 package org.example.javaFundamentals.november.dvanasty;
 
 public class BinarySearch {
-    static int[] pole = new int[]{2, 7, 8, 12, 44, 65, 88, 100, 120, 160, 300};
+    static int[] pole = new int[]{2, 7, 8, 12, 44, 65, 80, 100, 120, 160, 300};
 
-    /*public static void main(String[] args) {
-        int hledany=160;
-        for (int i=0; i<pole.length;i++){
-            if (hledany==pole[i]){
+    public static void main(String[] args) {
+        int hledany = 160;
+        for (int i = 0; i < pole.length; i++) {
+            if (hledany == pole[i]) {
                 System.out.println(i);
             }
         }
-        System.out.println(binarySearch(pole,160));
+        System.out.println(binarySearch(pole, 160, 0, pole.length));
+        System.out.println(binarySearch(pole, 8, 0, pole.length));
+        System.out.println(binarySearch(pole, 150, 0, pole.length));
     }
-    static int binarySearch(int[] pole, int hledany){
-        int median = pole.length/2;
-        if (pole[median]==hledany){
-            return median;
-        }*/
-}
 
+    static int binarySearch(int[] pole, int hledany, int leftIndex, int rightIndex) {
+        int median = ((rightIndex - leftIndex) / 2) + leftIndex;
+        if(leftIndex > rightIndex | leftIndex < 0){
+            return -1;
+        }
+        if (pole[median] == hledany) {
+            return median;
+        }
+        if( hledany < pole[median] ){
+            return  binarySearch(pole, hledany, leftIndex, median -1 );
+        }
+        else {
+            //je mensi
+            return binarySearch(pole, hledany, median + 1, rightIndex);
+        }
+
+    }
+}
--- src/main/java/org/example/javaFundamentals/november/dvanasty/Recursion.java
+++ src/main/java/org/example/javaFundamentals/november/dvanasty/Recursion.java
@@ -2,6 +2,58 @@ package org.example.javaFundamentals.november.dvanasty;
 
 public class Recursion {
     public static void main(String[] args) {
+        System.out.println(factRec(6));
+        int i = inc(5);
+        System.out.println();
+    }
 
+    static int fact(int n) {
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    static int factRec(int n) {
+
+        //4! = 1*2*3*4
+        //3! = 1*2*3
+        //4! = 3!*4
+        //3!= 2!*3
+        //2! = 1!*2
+        //1! = 1;
+        //n! = (n-1)!*n
+        if (n == 1) {//kazda rekurze musi v urcite chvili koncit
+            return 1;
+        }
+        int nMinus1Fact = factRec(n - 1);
+
+        int ret = nMinus1Fact * n;
+        System.out.println(n + "! =" + ret);
+        return ret;
+    }
+
+    static int factRecNice(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return factRecNice(n - 1) * n;
+        }
+    }
+
+    static int factRecVeryNice(int n) {
+        return (n == 1) ? 1 : factRecNice(n - 1) * n;
+        //        if (n == 1) {
+//            return 1;
+//        } else {
+//            return factRecNice(n - 1) * n;
+//        }
+    }
+
+    static int inc(int n) {
+        return n + 1;
     }
 }
+// skončila som 0h 19m
